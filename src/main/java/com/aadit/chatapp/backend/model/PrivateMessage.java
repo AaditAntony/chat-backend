6b1eedcb3d434faa3b4061134979895fa5c47b89
Aadit Antony <aaditantony1112@gmail.com>
2025-11-24 14:46:37 +0530

    the model has bee created

---
+++ src/main/java/com/aadit/chatapp/backend/model/PrivateMessage.java
@@ -0,0 +1,14 @@
+package com.aadit.chatapp.backend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PrivateMessage {
+    private String sender;
+    private String receiver;
+    private String content;
+}
